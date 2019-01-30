6d610c5236280379514c3b334d5ba0babb466534
Мария Гнутова <mariagnutova@MacBook-Pro.local>
2019-01-30 20:49:20 +0100

    Reverse String make in one line

--- java/reverse-string/src/main/java/ReverseString.java
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -1,13 +1,6 @@
 class ReverseString {
 
     String reverse(String inputString) {
-        StringBuilder revertString = new StringBuilder();
-        if (!(inputString==null)){
-            for (int i = inputString.length()-1; i >= 0; i--){
-                revertString.append(String.valueOf(inputString.charAt(i)));
-            }
-        }
-        return revertString.toString();
+        return new StringBuilder(inputString).reverse().toString();
     }
-  
 }
